999b71d785cbf67b6ad46f6d3573ab8da1dc6d07
Saleh Amoo <157945484+Amoo189@users.noreply.github.com>
2026-01-25 20:38:44 +0330

    ii6k.js

--- ii6k.js
+++ ii6k.js
@@ -618,7 +618,7 @@ function openSystem(){ openM(`<h3 style="color:var(--neon-cyan)">Quantum System<
 
 
 function shutdownSystem(){
- document.body.innerHTML=`<div style="background:black;color:#ff3b3b;height:100vh;display:flex;align-items:center;justify-content:center;font-size:1.5rem;text-shadow:0 0 30px #ff3b3b;">System is shutting down...</div>`;
+ document.body.innerHTML=`<div style="background:black;color:#ff3b3b;height:100vh;display:flex;align-items:center;justify-content:center;font-size:1.5rem;text-shadow:0 0 30px #ff3b3b;">Refresh this page>>System is shutting down...</div>`;
 }
 
 
